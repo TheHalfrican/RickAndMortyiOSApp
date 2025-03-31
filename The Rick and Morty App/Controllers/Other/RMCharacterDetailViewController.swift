4186e00475c38078d9d6f07b7675f722335a404f
Noah Mattocks <noahmattocks@gmail.com>
2025-03-31 14:10:36 -0400

    Set up The Character Detail View's Compositional Layout

--- The Rick and Morty App/Controllers/Other/RMCharacterDetailViewController.swift
+++ The Rick and Morty App/Controllers/Other/RMCharacterDetailViewController.swift
@@ -2,7 +2,7 @@
 //  RMCharacterDetailViewController.swift
 //  The Rick and Morty App
 //
-//  Created by Owner on 3/17/25.
+//  Created by Owner on 3/17/25. 
 //
 
 import UIKit
@@ -11,8 +11,13 @@ import UIKit
 final class RMCharacterDetailViewController: UIViewController {
     private let viewModel: RMCharacterDetailViewViewModel
     
+    private let detailView: RMCharacterDetailView
+    
+    // MARK: - Init
+    
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
+        self.detailView = RMCharacterDetailView(frame: .zero, viewModel: viewModel)
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -26,6 +31,56 @@ final class RMCharacterDetailViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = viewModel.title
+        view.addSubview(detailView)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .action,
+            target: self,
+            action: #selector(didTapShare)
+        )
+        addConstraints()
+        
+        detailView.collectionView?.delegate = self
+        detailView.collectionView?.dataSource = self
     }
+    
+    
+    @objc
+    private func didTapShare() {
+        // Share Character information
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            detailView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+}
 
+// MARK: - CollectionView
+
+extension RMCharacterDetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return viewModel.sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) ->
+    UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        if indexPath.section == 0 {
+            cell.backgroundColor = .systemMint
+        } else if indexPath.section == 1 {
+            cell.backgroundColor = .systemTeal
+        } else {
+            cell.backgroundColor = .systemIndigo
+        }
+        return cell
+    }
 }
