5bbe837be3c882cdd34392f602e7b7865111befa
Noah Mattocks <noahmattocks@gmail.com>
2025-04-21 14:23:24 -0400

    Added the Location Details View Page

--- The Rick and Morty App/Controllers/Core/RMLocationViewController.swift
+++ The Rick and Morty App/Controllers/Core/RMLocationViewController.swift
@@ -7,20 +7,18 @@
 
 import UIKit
 
-/// Controller to search for and show Locations
-final class RMLocationViewController: UIViewController, RMLocationViewViewModelDelegate {
-    func didFetchInitialLocations() {
-        primaryView.configure(with: viewModel)    }
-    
-    
+/// Controller to show and search for Locations
+final class RMLocationViewController: UIViewController, RMLocationViewViewModelDelegate, RMLocationViewDelegate {
+
     private let primaryView = RMLocationView()
-    
+
     private let viewModel = RMLocationViewViewModel()
-    
+
     // MARK: - Lifecycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        primaryView.delegate = self
         view.addSubview(primaryView)
         view.backgroundColor = .systemBackground
         title = "Locations"
@@ -29,29 +27,37 @@ final class RMLocationViewController: UIViewController, RMLocationViewViewModelD
         viewModel.delegate = self
         viewModel.fetchLocations()
     }
-    
+
     private func addSearchButton() {
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action:
-                                                                #selector(didTapSearch))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(didTapSearch))
     }
-    
+
     private func addConstraints() {
         NSLayoutConstraint.activate([
             primaryView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             primaryView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
             primaryView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
-            primaryView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            primaryView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
     }
-    
+
     @objc private func didTapSearch() {
-        
+        let vc = RMSearchViewController(config: .init(type: .location))
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
     }
-    
+
+    // MARK: - RMLocationViewDelegate
+
+    func rmLocationView(_ locationView: RMLocationView, didSelect location: RMLocation) {
+        let vc = RMLocationDetailViewController(location: location)
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
     // MARK: - LocationViewModel Delegate
-    
-    func didFetchInintialLocations() {
+
+    func didFetchInitialLocations() {
         primaryView.configure(with: viewModel)
     }
-    
 }
