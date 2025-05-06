e321c9a45d4e2d492da170a47f8f62ee8d9dd943
Noah Mattocks <noahmattocks@gmail.com>
2025-05-06 12:44:29 -0400

    Created functionality for selecting search options

--- The Rick and Morty App/ViewModels/Search/RMSearchViewViewModel.swift
+++ The Rick and Morty App/ViewModels/Search/RMSearchViewViewModel.swift
@@ -16,8 +16,45 @@ final class RMSearchViewViewModel {
     
     let config: RMSearchViewController.Config
     
+    private var optionMap: [RMSearchInputViewViewModel.DynamicOption: String] = [:]
+     private var searchText = ""
+
+     private var optionMapUpdateBlock: (((RMSearchInputViewViewModel.DynamicOption, String)) -> Void)?
+
+    //  private var searchResultHandler: ((RMSearchResultViewModel) -> Void)?
+
+     private var noResultsHandler: (() -> Void)?
+
+     private var searchResultModel: Codable?
+
+     // MARK: - Init
+
     init(config: RMSearchViewController.Config) {
         self.config = config
+    }
+    
+    // MARK: - Public
+    
+    public func executeSearch() {
+        // Create Request based on filters
+        // Send API calls
+        // Notify view of results, no results, or error
+    }
+    
+    public func set(query text: String) {
+        self.searchText = text
+    }
+    
+    public func set(value: String, for option: RMSearchInputViewViewModel.DynamicOption) {
+        optionMap[option] = value
+        let tuple = (option, value)
+        optionMapUpdateBlock?(tuple)
+    }
+    
+    public func registerOptionChangeBlock(
+        _ block: @escaping ((RMSearchInputViewViewModel.DynamicOption, String)) -> Void
+    ) {
+        self.optionMapUpdateBlock = block
     }
 }
 
